e946d29ae2c913ad646ccd99a0b31af550c4248c
Emilio <emilioplatzer@gmail.com>
2015-07-22 17:47:16 -0300

    Primer caso de prueba con Karma

--- test/test.js
+++ test/test.js
@@ -1,12 +1,25 @@
 "use strict";
 
-var expect = require('expect.js');
-var qac = require('..');
+describe("ajax-best-promise fromElements", function() {
+    var fromElements = AjaxBestPromise.fromElements;
 
-describe('ajax-best-promise', function(){
-    describe('basic test', function(){
-        it('first test', function(){
-            throw new Error('no tests');
-        });
+    beforeEach(function() {
+        var fixture = '<div id=fixture><input id="param1" value="one">' + 
+          '<input id="param2" value="two"></div>';
+
+        document.body.insertAdjacentHTML(
+          'afterbegin', 
+          fixture);
     });
+  
+    afterEach(function() {
+        document.body.removeChild(document.getElementById('fixture'));
+    });
+    
+    it("get input values", function() {
+        expect(fromElements).to.be.ok();
+        expect(fromElements).to.be.a(Function);
+        expect(fromElements(['param1', 'param2'])).to.eql({param1:'one', param2:'two'});
+    });
+
 });
